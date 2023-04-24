00270ff47f4b549be561d46b11db1ba64f99e44e
Jesus Agustin Morales Marta <l18540431@gmail.com>
2023-04-24 10:02:58 -0600

    24/04/22

--- servidor.js
+++ servidor.js
@@ -20,3 +20,15 @@ var mimeTypes={
     '.jpg': 'image/jpg',
     '.gif': 'image/gif'
 };
+fs.readFile(filePatch, function(error, content)){
+    if (error){
+        if(error.code== 'ENOENT'){
+            fs.readFile('./404.html', function(error, content){
+                response.writeHead(200, {'Content-Type': contenttype});
+                response.end(content, 'utf-8');
+            });
+        }
+    }else{
+        response.writeHead(200, { 'Content-Type':})
+    }
+}
