b713f5c9cf6713d584ef89c0694ab76d59f7fe5b
Jesus Agustin Morales Marta <l18540431@gmail.com>
2023-04-24 09:52:38 -0600

    servidor de operaciónes completo

---
+++ servidor.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require
+var path = require('path');
+http.createServer(function (request, response){
+    console.log('request',request.url);
+    
+    var filePath = '.' + request.url;
+    if(filePath == './'){
+        filePath = './index.html';
+    }
+
+var extname= String(path.extname(filePath)).toLowerCase();
+var contentType='text/html';
+var mimeTypes={
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif'
+};
