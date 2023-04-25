1b4583cdeb6b098262d951b1523557319f607bca
Jesus Agustin Morales Marta <l18540431@gmail.com>
2023-04-25 09:11:49 -0600

    se agregaron las lineas faltantes al codigo

--- servidor.js
+++ servidor.js
@@ -20,7 +20,7 @@ var mimeTypes={
     '.jpg': 'image/jpg',
     '.gif': 'image/gif'
 };
-fs.readFile(filePatch, function(error, content)){
+fs.readFile(filePatch, function(error, content){
     if (error){
         if(error.code== 'ENOENT'){
             fs.readFile('./404.html', function(error, content){
@@ -29,6 +29,9 @@ fs.readFile(filePatch, function(error, content)){
             });
         }
     }else{
-        response.writeHead(200, { 'Content-Type':})
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
     }
-}
+});
+}).listen(3000);
+console.log('Server running at http:127.0.0.1:3000/');
